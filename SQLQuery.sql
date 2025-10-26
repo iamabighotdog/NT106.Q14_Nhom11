2e3622786d90162e90cbc5aa451e9fea09161d84
iamabighotdog <93434785+iamabighotdog@users.noreply.github.com>
2025-10-26 22:38:26 +0700

    hehe
    hehe

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,13 +1,16 @@
-CREATE DATABASE UserManagement
+IF DB_ID('UserAuthDB') IS NULL
+    CREATE DATABASE UserAuthDB;
 GO
-USE UserManagement
+USE UserAuthDB;
 GO
-CREATE TABLE Users (
-    UserId INT IDENTITY(1,1) PRIMARY KEY,
-    Username NVARCHAR(50) NOT NULL UNIQUE,
-    Password NVARCHAR(255) NOT NULL,
-    Email NVARCHAR(100) NOT NULL UNIQUE,
-    Phone NVARCHAR(20) NOT NULL UNIQUE,
-    Confirmed BIT NOT NULL DEFAULT 0
-); 
-
+IF OBJECT_ID('dbo.Users','U') IS NULL
+BEGIN
+    CREATE TABLE dbo.Users(
+        Id INT IDENTITY PRIMARY KEY,
+        Username NVARCHAR(50) NOT NULL,
+        Email NVARCHAR(100) NULL,
+        Phone NVARCHAR(20) NULL,
+        Password NVARCHAR(64) NOT NULL
+    );
+END
+GO
