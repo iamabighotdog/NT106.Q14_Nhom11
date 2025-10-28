f215a57322f51386f9846fc29285e063b3317674
hifs-tv <24520463@gm.uit.edu.vn>
2025-10-29 03:36:22 +0700

    SQL

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -3,14 +3,16 @@ IF DB_ID('UserAuthDB') IS NULL
 GO
 USE UserAuthDB;
 GO
+
 IF OBJECT_ID('dbo.Users','U') IS NULL
 BEGIN
     CREATE TABLE dbo.Users(
-        Id INT IDENTITY PRIMARY KEY,
-        Username NVARCHAR(50) NOT NULL,
-        Email NVARCHAR(100) NULL,
-        Phone NVARCHAR(20) NULL,
-        Password NVARCHAR(64) NOT NULL
+        UserId   INT IDENTITY(1,1) PRIMARY KEY,
+        Username NVARCHAR(50)  NOT NULL UNIQUE,
+        Email    NVARCHAR(100) NULL UNIQUE,
+        Phone NVARCHAR(20) NULL UNIQUE,
+        Password NVARCHAR(64) NOT NULL,           
+        FullName NVARCHAR(150) NULL,
+        Birthday DATE          NULL
     );
 END
-GO
