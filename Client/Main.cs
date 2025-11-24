f60d0be0266987e5a74a1f5f6ed947d0c77bf27f
Duynnuit <nhatduy189298@gmail.com>
2025-11-24 20:57:40 +0700

    add pic_Avatar

--- Client/Main.cs
+++ Client/Main.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.IO;
 using System.Text.Json;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace FormAppQuyt
 {
     public partial class Main : Form
     {
         private readonly string userInput;
+        private byte[] selectedImage = null;
 
         public Main(string input)
         {
@@ -62,6 +65,44 @@ namespace FormAppQuyt
         {
             createQuiz createQuiz = new createQuiz();
             createQuiz.ShowDialog();
+        }
+
+        private void guna2CirclePictureBox1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = ofd.FileName;
+                    FileInfo fileInfo = new FileInfo(filePath);
+
+                    const int MAX_FILE_SIZE_BYTES = 500 * 1024;
+
+                    if (fileInfo.Length > MAX_FILE_SIZE_BYTES)
+                    {
+                        MessageBox.Show("Kích thước file ảnh quá lớn (> 500KB). Vui lòng chọn ảnh nhỏ hơn.",
+                                        "Cảnh báo Kích thước", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        selectedImage = null;
+                        return;
+                    }
+
+                    try
+                    {
+
+                        selectedImage = File.ReadAllBytes(filePath);
+
+                        using (MemoryStream ms = new MemoryStream(selectedImage))
+                        {
+                            pic_Avatar.Image = Image.FromStream(ms);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Lỗi đọc file: " + ex.Message, "Lỗi");
+                    }
+                }
+            }
         }
     }
 }
